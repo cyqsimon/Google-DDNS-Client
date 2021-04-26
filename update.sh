9a5e5857eeccee42eb8faaf2213173cccaac9a65
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2021-04-27 04:22:57 +0800

    Added granularity to HTTP status code handling

--- update.sh
+++ update.sh
@@ -12,6 +12,7 @@ hostname="CHANGE_ME" # your fully qualified domain name (FQDN)
 username="CHANGE_ME" # your username, provided by DDNS service provider
 password="CHANGE_ME" # your password, provided by DDNS service provider
 ddns_api_proxy="CHANGE_ME" # proxy used for DDNS API in cURL format; set to "" if proxy is unnecessary
+tmp_path="/tmp/google-ddns-client" # path to temp file
 log_path="./log.txt" # path to log file
 
 # go to script's source dir
@@ -40,12 +41,17 @@ if [[ "$dns_public_ip" == "" ]]; then
 fi
 
 # get actual IP
-actual_public_ip=`curl --silent --fail "https://api.ipify.org"`
+curl_status=`curl --silent --output "$tmp_path" --write-out "%{http_code}" "https://api.ipify.org"`
 curl_exit_code="$?"
 if [[ "$curl_exit_code" != "0" ]]; then
   echo "G-DDNS: [$(date +"%F %T")] Cannot get current IP via ipify API (cURL error $curl_exit_code); exiting; will keep retrying" | tee -a "$log_path"
   exit 4
 fi
+if (( $curl_status > 399 )); then
+  echo "G-DDNS: [$(date +"%F %T")] Cannot get current IP via ipify API (bad response $curl_status); exiting; will keep retrying" | tee -a "$log_path"
+  exit 4
+fi
+actual_public_ip=`cat "$tmp_path"`
 
 # check for IP change
 if [[ "$dns_public_ip" == "$actual_public_ip" ]]; then
@@ -59,7 +65,7 @@ export https_proxy=$ddns_api_proxy
 
 # send update request to DDNS API
 req_url="https://$username:$password@domains.google.com/nic/update?hostname=$hostname&myip=$actual_public_ip"
-ddns_res=`curl --silent --fail "$req_url"`
+curl_status=`curl --silent --output "$tmp_path" --write-out "%{http_code}" "$req_url"`
 curl_exit_code="$?"
 echo "G-DDNS: [$(date +"%F %T")] Update request sent:" | tee -a "$log_path"
 echo -e "G-DDNS: [$(date +"%F %T")] \t$req_url" | tee -a "$log_path"
@@ -67,6 +73,11 @@ if [[ "$curl_exit_code" != "0" ]]; then
   echo "G-DDNS: [$(date +"%F %T")] Update request via G-DDNS API failed (cURL error $curl_exit_code); exiting; will keep retrying" | tee -a "$log_path"
   exit 4
 fi
+if (( $curl_status > 399 )); then
+  echo "G-DDNS: [$(date +"%F %T")] Update request via G-DDNS API failed (bad response $curl_status); exiting; will keep retrying" | tee -a "$log_path"
+  exit 4
+fi
+ddns_res=`cat "$tmp_path"`
 
 # handle API response
 if [[ "$ddns_res" =~ "good" ]]; then
