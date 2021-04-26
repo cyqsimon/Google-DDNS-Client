678667a9817582139cf9477872989d1e2a48ab6d
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2021-04-26 19:55:39 +0800

    Fixed incorrect command substitution

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ cd `dirname "$0"`
 
 # exit if script has previously errored
 if [ -f "./script_error" ]; then
-  echo "G-DDNS: [${date +"%F %T"}] Script has previously errored; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] Script has previously errored; exiting"
   exit 3
 fi
 
@@ -33,13 +33,13 @@ dns_public_ip=`dig +short "$hostname"`
 actual_public_ip=`curl --silent --fail "https://api.ipify.org"`
 curl_exit_code="$?"
 if [[ "$curl_exit_code" != "0" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] Cannot get current IP via API (cURL error $curl_exit_code); exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] Cannot get current IP via API (cURL error $curl_exit_code); exiting; will keep retrying"
   exit 4
 fi
 
 # check for IP change
 if [[ "$dns_public_ip" == "$actual_public_ip" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] Public IP has not changed: $dns_public_ip"
+  echo "G-DDNS: [$(date +"%F %T")] Public IP has not changed: $dns_public_ip"
   exit 0
 fi
 
@@ -51,25 +51,25 @@ export https_proxy=$ddns_api_proxy
 req_url="https://$username:$password@domains.google.com/nic/update?hostname=$hostname&myip=$actual_public_ip"
 ddns_res=`curl --silent --fail "$req_url"`
 curl_exit_code="$?"
-echo "G-DDNS: [${date +"%F %T"}] Update request sent:"
-echo -e "G-DDNS: [${date +"%F %T"}] \t$req_url"
+echo "G-DDNS: [$(date +"%F %T")] Update request sent:"
+echo -e "G-DDNS: [$(date +"%F %T")] \t$req_url"
 if [[ "$curl_exit_code" != "0" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] Update request via API failed (cURL error $curl_exit_code); exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] Update request via API failed (cURL error $curl_exit_code); exiting; will keep retrying"
   exit 4
 fi
 
 # handle API response
 if [[ "$ddns_res" =~ "good" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] Public IP successfully updated from $dns_public_ip to $actual_public_ip; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] Public IP successfully updated from $dns_public_ip to $actual_public_ip; exiting"
   exit 0
 elif [[ "$ddns_res" =~ "nochg" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] API reports public IP has not changed: $actual_public_ip; please wait for DNS record to propagate; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] API reports public IP has not changed: $actual_public_ip; please wait for DNS record to propagate; exiting"
   exit 0
 elif [[ "$ddns_res" =~ "911" ]]; then
-  echo "G-DDNS: [${date +"%F %T"}] API has errored; exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] API has errored; exiting; will keep retrying"
   exit 1
 else
-  echo "[${date +"%F %T"}] Error reason: $ddns_res" > "./script_error"
-  echo "G-DDNS: [${date +"%F %T"}] API reports request error: $ddns_res; exiting; will stop retrying"
+  echo "[$(date +"%F %T")] Error reason: $ddns_res" > "./script_error"
+  echo "G-DDNS: [$(date +"%F %T")] API reports request error: $ddns_res; exiting; will stop retrying"
   exit 2
 fi
