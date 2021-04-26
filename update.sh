da52dc65afcdac03490231d430942a06d3bf024d
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2021-04-27 03:48:26 +0800

    tee stdout to logfile

--- update.sh
+++ update.sh
@@ -12,13 +12,14 @@ hostname="CHANGE_ME" # your fully qualified domain name (FQDN)
 username="CHANGE_ME" # your username, provided by DDNS service provider
 password="CHANGE_ME" # your password, provided by DDNS service provider
 ddns_api_proxy="CHANGE_ME" # proxy used for DDNS API in cURL format; set to "" if proxy is unnecessary
+log_path="./log.txt" # path to log file
 
 # go to script's source dir
 cd `dirname "$0"`
 
 # exit if script has previously errored
 if [ -f "./script_error" ]; then
-  echo "G-DDNS: [$(date +"%F %T")] Script has previously errored; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] Script has previously errored; exiting" | tee -a "$log_path"
   exit 3
 fi
 
@@ -30,25 +31,25 @@ export https_proxy=""
 dns_records=`dig +short "$hostname"`
 dig_exit_code="$?"
 if [[ "$dig_exit_code" != "0" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Cannot get current DNS mapping (dig error $dig_exit_code); exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] Cannot get current DNS mapping (dig error $dig_exit_code); exiting; will keep retrying" | tee -a "$log_path"
   exit 4
 fi
 dns_public_ip=`echo "$dns_records" | grep -v ":" | head -1` # filter out IPv6 and use 1st entry
 if [[ "$dns_public_ip" == "" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Found no current DNS mapping"
+  echo "G-DDNS: [$(date +"%F %T")] Found no current DNS mapping" | tee -a "$log_path"
 fi
 
 # get actual IP
 actual_public_ip=`curl --silent --fail "https://api.ipify.org"`
 curl_exit_code="$?"
 if [[ "$curl_exit_code" != "0" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Cannot get current IP via API (cURL error $curl_exit_code); exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] Cannot get current IP via API (cURL error $curl_exit_code); exiting; will keep retrying" | tee -a "$log_path"
   exit 4
 fi
 
 # check for IP change
 if [[ "$dns_public_ip" == "$actual_public_ip" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Public IP has not changed: $dns_public_ip"
+  echo "G-DDNS: [$(date +"%F %T")] Public IP has not changed: $dns_public_ip" | tee -a "$log_path"
   exit 0
 fi
 
@@ -60,25 +61,25 @@ export https_proxy=$ddns_api_proxy
 req_url="https://$username:$password@domains.google.com/nic/update?hostname=$hostname&myip=$actual_public_ip"
 ddns_res=`curl --silent --fail "$req_url"`
 curl_exit_code="$?"
-echo "G-DDNS: [$(date +"%F %T")] Update request sent:"
-echo -e "G-DDNS: [$(date +"%F %T")] \t$req_url"
+echo "G-DDNS: [$(date +"%F %T")] Update request sent:" | tee -a "$log_path"
+echo -e "G-DDNS: [$(date +"%F %T")] \t$req_url" | tee -a "$log_path"
 if [[ "$curl_exit_code" != "0" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Update request via API failed (cURL error $curl_exit_code); exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] Update request via API failed (cURL error $curl_exit_code); exiting; will keep retrying" | tee -a "$log_path"
   exit 4
 fi
 
 # handle API response
 if [[ "$ddns_res" =~ "good" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] Public IP successfully updated from $dns_public_ip to $actual_public_ip; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] Public IP successfully updated from $dns_public_ip to $actual_public_ip; exiting" | tee -a "$log_path"
   exit 0
 elif [[ "$ddns_res" =~ "nochg" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] API reports public IP has not changed: $actual_public_ip; please wait for DNS record to propagate; exiting"
+  echo "G-DDNS: [$(date +"%F %T")] API reports public IP has not changed: $actual_public_ip; please wait for DNS record to propagate; exiting" | tee -a "$log_path"
   exit 0
 elif [[ "$ddns_res" =~ "911" ]]; then
-  echo "G-DDNS: [$(date +"%F %T")] API has errored; exiting; will keep retrying"
+  echo "G-DDNS: [$(date +"%F %T")] API has errored; exiting; will keep retrying" | tee -a "$log_path"
   exit 1
 else
-  echo "[$(date +"%F %T")] Error reason: $ddns_res" > "./script_error"
-  echo "G-DDNS: [$(date +"%F %T")] API reports request error: $ddns_res; exiting; will stop retrying"
+  touch "./script_error"
+  echo "G-DDNS: [$(date +"%F %T")] API reports request error: $ddns_res; exiting; will stop retrying" | tee -a "$log_path"
   exit 2
 fi
